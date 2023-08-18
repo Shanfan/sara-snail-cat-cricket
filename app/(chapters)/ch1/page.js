b6b7abf3fc4ef18bbd08b346ee3665a370012511
Shanfan Huang <Shanfan@users.noreply.github.com>
2023-08-18 16:16:31 -0400

    Better CSS organization

--- app/(chapters)/ch1/page.js
+++ app/(chapters)/ch1/page.js
@@ -20,29 +20,38 @@ const sedgwick = Sedgwick_Ave({
     weight: '400'
 })
 
+//Utility CSS Classes
+const { row, wide, narrow, white, black, layers,
+    narration, dialog, hero, panelL, panelM, panelS
+} = styles
+
 export default function Ch1() {
     return (
         <>
-            <div className={`${styles.row} ${styles.black}`}>
+            <div className={`${row} ${black}`}>
                 <ChapterHeader
                     currentChapter={chapters.one}
                     style={styles.chapterHeader} />
             </div>
             <article className={sedgwick.className}>
-                <div className={`${styles.row} ${styles.black}`}>
-                    <div className={styles.narrow}>
-                        <p className={styles.narration}>
-                            It was a crispy summer night.<br />
-                            The air finally cooled down.<br />
-                            The grassland glistened with dewdrops.<br />
+                <div className={`${row} ${black}`}>
+                    <div className={narrow}>
+                        <p className={narration}>
+                            It was a crispy summer night.
+                            The air finally cooled down.
+                            The grassland glistened with dewdrops.
                         </p>
                     </div>
-                    <div className={styles.wide}>
+                    <div className={wide}>
                         <Image
                             alt="A drawing of a snail sleeping on top of a leaf. A dewdrop drapes down another grass leaf. Moon hangs in the starry sky."
                             src={images['./ch1_sn00_pn00.jpg']}
-                            className={styles.hero}
+                            className={hero}
+                            priority
                         />
+                    </div>
+                    <div className={narrow}>
+
                     </div>
                 </div>
             </article>
