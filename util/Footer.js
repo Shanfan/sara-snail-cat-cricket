302d424c9bf08b2ef0d5bf36e376fa92c888a0df
Shanfan Huang <Shanfan@users.noreply.github.com>
2023-09-02 09:50:21 -0400

    Replacing copyright information with Creative Common license

--- util/Footer.js
+++ util/Footer.js
@@ -1,10 +1,44 @@
+import Image from "next/image"
+import cc from "@/public/cc-logo.svg"
+import by from "@/public/cc-by.svg"
+import nc from "@/public/cc-nc.svg"
+
 export default function Footer() {
-    const currentDate = new Date();
     return (
         <footer>
-            <p>
-                &copy; Copyright Shanfan Huang <span>{currentDate.getFullYear()}</span>
-            </p>
+
+            <div style={{
+                display: 'inline-flex',
+                alignItems: 'center',
+                padding: '0.7em',
+            }}>
+                <Image
+                    src={cc}
+                    height={20}
+                    alt=""
+                />
+                &nbsp;
+                <Image
+                    src={by}
+                    height={20}
+                    alt=""
+                />
+                &nbsp;
+                <Image
+                    src={nc}
+                    height={20}
+                    alt=""
+                />
+            </div>
+            by&nbsp;
+            <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="https://github.com/shanfan">
+                Shanfan Huang
+            </a>
+            &nbsp;and licensed under&nbsp;
+            <a href="http://creativecommons.org/licenses/by-nc/4.0/?ref=chooser-v1" target="_blank">
+                CC BY-NC 4.0
+            </a>
+
         </footer>
     )
 }
